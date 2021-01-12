b074fa387c29df9a0d6f33cd7e0d6e7686d7ae20
Kenneth Ngo <kenneth.ngo@entur.org>
2021-01-12 18:16:18 +0100

    add yarn lock and tsconfig

--- src/api/apiHelper.ts
+++ src/api/apiHelper.ts
@@ -1,4 +1,5 @@
 import fetch from 'isomorphic-unfetch'
 
+// eslint-disable-next-line no-undef
 export const fetcher = (path: string, options?: RequestInit) =>
   fetch(path, options).then((res) => res.json())
